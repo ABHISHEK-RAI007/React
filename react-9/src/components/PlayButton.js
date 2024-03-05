b5415d06ff62d7457b47a68ee35051861cadfd65
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2024-03-05 12:10:26 +0530

    Ch 9 - Context API , useContext hook

---
+++ react-9/src/components/PlayButton.js
@@ -0,0 +1,26 @@
+import ThemeContext from '../context/ThemeContext';
+import './PlayButton.css';
+import { useContext, useState } from 'react';
+
+function PlayButton({message,children,onPlay,onPause}){
+    console.log('render PlayButton')
+    const theme = useContext(ThemeContext)
+
+    const [playing, setPlaying] = useState(false);  
+    function handleClick(e){
+        // console.log(e)
+        e.stopPropagation()
+
+        if(playing) onPause()
+        else onPlay();
+
+        setPlaying(!playing);
+    }
+
+    return (
+        <button className={theme} onClick={handleClick}>{children} : {playing?'⏸️':'⏯️'}</button>
+    )
+
+}
+
+export default PlayButton;
