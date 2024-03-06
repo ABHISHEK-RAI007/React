50189e8054d0da6c83ff39736a0767014e03916c
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2024-03-06 21:14:25 +0530

    Ch 10 - Context API with useReducers, Custom hooks

---
+++ react-10/src/hooks/Video.js
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+import VideosContext from "../context/VideosContext";
+
+
+function useVideos(){
+    const  dispatch = useContext(VideosContext)
+    return dispatch
+}
+
+export default useVideos;
